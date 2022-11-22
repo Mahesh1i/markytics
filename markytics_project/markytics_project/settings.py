41f067f2712e8312103641ba99167a0be69bffb5
Mahesh1i <mkamble0245@gmail.com>
2022-11-22 15:29:18 +0530

    Few changes

--- markytics_project/markytics_project/settings.py
+++ markytics_project/markytics_project/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import environ
+
 
 from pathlib import Path
 
@@ -19,11 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%vc#+q7s-y1@#2s0v-(koau(yr@n=rb&u%o^h%3y24_s-45g7m'
+env = environ.Env()
+SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
